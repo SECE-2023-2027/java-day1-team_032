670087fe6de9313e9c92c1eb0d2e3804450cd529
MADHUMATHI V E <mathsmaddy691@gmail.com>
2026-06-15 15:42:31 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+public static void main(String[] args) {
+    System.out.println("Enter your name:");
+    Scanner sc=new Scanner(System.in);
+    String name;
+    name=sc.next();
+    System.out.println("Name:"+name);
+    sc.close();
+}
+}
